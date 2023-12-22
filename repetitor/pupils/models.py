0a9a0d6c899d65a980c686f489202a570556b9bd
Andrew Litvinov <lita.private@gmail.com>
2023-12-22 17:40:43 +0300

    Change db structure

--- repetitor/pupils/models.py
+++ repetitor/pupils/models.py
@@ -3,16 +3,6 @@ from django.db import models
 
 # Create your models here.
 
-class Pupil(models.Model):
-    name = models.CharField(max_length=50)
-    school_number = models.IntegerField()
-    school_class = models.CharField(max_length=5)
-    is_active = models.BooleanField()
-
-    def __str__(self):
-        return f'{self.name} СШ № {self.school_number} класс {self.school_class} '
-
-
 class School(models.Model):
     school_number = models.IntegerField()
 
@@ -23,10 +13,20 @@ class SchoolClass(models.Model):
     class_letter = models.CharField(max_length=1)
 
 
+class Pupil(models.Model):
+    name = models.CharField(max_length=50)
+    school_number = models.ForeignKey(School, on_delete=models.SET_NULL, null=True)
+    school_class = models.ForeignKey(SchoolClass, on_delete=models.SET_NULL, null=True)
+    is_active = models.BooleanField()
+
+    def __str__(self):
+        return f'{self.name} СШ № {self.school_number} класс {self.school_class} '
+        # return f'{self.name}  '
+
+
 class Lesson(models.Model):
     pupil = models.ForeignKey(Pupil, on_delete=models.CASCADE)
     date = models.DateTimeField()
     lesson_title = models.CharField(max_length=20)
     price = models.IntegerField(default=0)
     lesson_comment = models.TextField(blank=True)
-
